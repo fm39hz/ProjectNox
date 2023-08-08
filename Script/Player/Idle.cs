e3a76fc8d00fed42115fe1b074365e5f0a054c7b
fm39hz <hitpoint2k3@gmail.com>
2023-08-08 14:37:49 +0700

    reformat code

--- Script/Player/Idle.cs
+++ Script/Player/Idle.cs
@@ -1,21 +1,31 @@
 using System;
 using GameSystem.Component.FiniteStateMachine;
 
-namespace  Actor.Attach.Player;
-	public partial class Idle : DynamicState{
-		public override void _Ready(){
-			base._Ready();
-			var _inputManager = Object.InputManager;
-			_inputManager.MovementKeyPressed += this.SetCondition;
-			}
-		public override void SetCondition(bool condition){
-			if (!this.IsInitialized){
-				return;
-				}
-			Condition = !condition;
-			}
-		public override void RunningState(double delta){
-			base.RunningState(delta);
-			Object.Velocity = Object.Velocity.MoveToward(Object.Information.Direction.AsVector * this.MaxSpeed, Friction * Convert.ToSingle(delta));
-			}
+namespace Actor.Attach.Player;
+
+public partial class Idle : DynamicState
+{
+	public override void _Ready()
+	{
+		base._Ready();
+		var _inputManager = Object.InputManager;
+		_inputManager.MovementKeyPressed += SetCondition;
+	}
+
+	public override void SetCondition(bool condition)
+	{
+		if (!IsInitialized)
+		{
+			return;
 		}
+
+		Condition = !condition;
+	}
+
+	public override void RunningState(double delta)
+	{
+		base.RunningState(delta);
+		Object.Velocity = Object.Velocity.MoveToward(Object.Information.Direction.AsVector * MaxSpeed,
+			Friction * Convert.ToSingle(delta));
+	}
+}
--- Script/Player/Walk.cs
+++ Script/Player/Walk.cs
@@ -1,20 +1,26 @@
 using System;
 using GameSystem.Component.FiniteStateMachine;
 
-namespace  Actor.Attach.Player;	
-	public partial class Walk : DynamicState{
-		public override void _Ready(){
-			base._Ready();
-			Object.InputManager.MovementKeyPressed += this.SetCondition;
-			}
-		// public override void SetCondition(bool condition){
-		// 	base.SetCondition(condition);
-		// 	if (Object.Velocity.IsZeroApprox() && !StateController.PreviousState.IsState(this)){
-		// 		Condition = false;
-		// 		}
-		// 	}
-		public override void RunningState(double delta){
-			base.RunningState(delta);
-			Object.Velocity = Object.Velocity.MoveToward(Object.InputManager.TopDownVector(Object.Velocity) * this.MaxSpeed, Acceleration * Convert.ToSingle(delta));
-			}
-		}
+namespace Actor.Attach.Player;
+
+public partial class Walk : DynamicState
+{
+	public override void _Ready()
+	{
+		base._Ready();
+		Object.InputManager.MovementKeyPressed += SetCondition;
+	}
+
+	// public override void SetCondition(bool condition){
+	// 	base.SetCondition(condition);
+	// 	if (Object.Velocity.IsZeroApprox() && !StateController.PreviousState.IsState(this)){
+	// 		Condition = false;
+	// 		}
+	// 	}
+	public override void RunningState(double delta)
+	{
+		base.RunningState(delta);
+		Object.Velocity = Object.Velocity.MoveToward(Object.InputManager.TopDownVector(Object.Velocity) * MaxSpeed,
+			Acceleration * Convert.ToSingle(delta));
+	}
+}
